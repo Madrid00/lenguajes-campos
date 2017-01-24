d56b2ebf9b490e69245c637c793c2970de7eb34e
Cesar <Cesar@10.20.5.191>
2017-01-23 19:49:07 -0600

    Ejerciciio Nomina2

---
+++ lenguajes/src/nominas2/Nomina.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nominas2;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Nomina {
+    
+    Trabajador worker;
+    Float saldo;
+    Date fechaPago;
+    
+}
---
+++ lenguajes/src/nominas2/Trabajador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nominas2;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Trabajador {
+    String nombre;
+    String paterno;
+    long seguro;
+    
+}
