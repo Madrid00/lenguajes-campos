abbcfeb8f65c5809a82de16a333c1cf8307051ac
T-102 <T-102@PC196>
2017-02-02 19:36:16 -0600

    Poliformismo y Herencia

---
+++ lenguajes/src/patos/PatoEuropeo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patos;
+
+/**
+ *
+ * @author T-102
+ */
+public class PatoEuropeo extends Pato implements ComportamientoVolar{
+    public static final String color="Negro";
+
+    @Override
+    public void volar() {
+        System.out.println("Soy un pato europeo y vuelo");
+    }
+    
+    
+}
